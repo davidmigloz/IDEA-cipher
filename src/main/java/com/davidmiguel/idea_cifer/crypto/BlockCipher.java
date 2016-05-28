906000b2f3d30d8d236f8bd8a627d30b63c915b6
davidmigloz <me@davidmiguel.com>
2016-05-28 17:57:08 +0200

    add BlockCipher.java

---
+++ src/main/java/com/davidmiguel/idea_cifer/crypto/BlockCipher.java
@@ -0,0 +1,40 @@
+package com.davidmiguel.idea_cifer.crypto;
+
+/**
+ * Created by davidmigloz on 28/05/2016.
+ */
+public abstract class BlockCipher extends Cipher {
+
+    private int blockSize;
+
+    public BlockCipher(int keySize, int blockSize) {
+        super(keySize);
+        this.blockSize = blockSize;
+    }
+
+    public int getBlockSize() {
+        return blockSize;
+    }
+
+    /**
+     * Encrypt a block of bytes.
+     */
+    public abstract void encrypt(byte[] clearText, int clearOff, byte[] cipherText, int cipherOff);
+
+    /* Decrypt a block of bytes. */
+    public abstract void decrypt(byte[] cipherText, int cipherOff, byte[] clearText, int clearOff);
+
+    /**
+     * Encrypt a block of bytes.
+     */
+    public void encrypt(byte[] clearText, byte[] cipherText) {
+        encrypt(clearText, 0, cipherText, 0);
+    }
+
+    /**
+     * Decrypt a block of bytes.
+     */
+    public void decrypt(byte[] cipherText, byte[] clearText) {
+        decrypt(cipherText, 0, clearText, 0);
+    }
+}
