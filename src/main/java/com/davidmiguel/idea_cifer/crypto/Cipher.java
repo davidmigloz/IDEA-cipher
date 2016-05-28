5bf89326aed0073a606f05b414a733e7f5753c21
davidmigloz <me@davidmiguel.com>
2016-05-28 17:52:46 +0200

    Add Cipher.java

--- src/main/java/com/davidmiguel/idea_cifer/crypto/Cipher.java
+++ src/main/java/com/davidmiguel/idea_cifer/crypto/Cipher.java
@@ -5,9 +5,40 @@ package com.davidmiguel.idea_cifer.crypto;
  */
 public abstract class Cipher {
 
-    public int keySize;
+    private int keySize;
 
     public Cipher(int keySize) {
         this.keySize = keySize;
+    }
+
+    public int getKeySize() {
+        return keySize;
+    }
+
+    /**
+     * Set the key from a block of bytes.
+     */
+    public abstract void setKey(byte[] key);
+
+    /**
+     * Set the key from a string.
+     */
+    public void setKey(String keyStr) {
+        setKey(makeKey(keyStr));
+    }
+
+    /**
+     * Turn a string into a key of the right length.
+     */
+    public byte[] makeKey(String keyStr) {
+        byte[] key = new byte[keySize];
+        int i, j;
+        for (j = 0; j < key.length; ++j) {
+            key[j] = 0;
+        }
+        for (i = 0, j = 0; i < keyStr.length(); i++, j = (j + 1) % key.length) {
+            key[j] ^= (byte) keyStr.charAt(i);
+        }
+        return key;
     }
 }
