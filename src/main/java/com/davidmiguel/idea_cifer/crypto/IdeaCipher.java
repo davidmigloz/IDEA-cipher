2e2baf1704cf26cc88254759156553fd1b27a651
davidmigloz <me@davidmiguel.com>
2016-05-29 23:58:39 +0200

    Implement IDEA group operations

--- src/main/java/com/davidmiguel/idea_cifer/crypto/IdeaCipher.java
+++ src/main/java/com/davidmiguel/idea_cifer/crypto/IdeaCipher.java
@@ -49,7 +49,7 @@ public class IdeaCipher extends BlockCipher {
         for (int i = 0; i < userKey.length / 2; i++) {
             b1 = (userKey[2 * i] & 0xFF) << 8;  // xxxxxxxx00000000
             b2 = userKey[2 * i + 1] & 0xFF;     // 00000000xxxxxxxx
-            key[i] = (b1 | b2);         // xxxxxxxxxxxxxxxx
+            key[i] = (b1 | b2);                 // xxxxxxxxxxxxxxxx
         }
 
         // The key is rotated 25 bits to the left and again divided into eight subkeys.
@@ -73,5 +73,61 @@ public class IdeaCipher extends BlockCipher {
     private int[] invertSubKey(int[] subkey) {
 
         return null;
+    }
+
+    /**
+     * Addition in the additive group (mod 2^16).
+     * Range [0, 0xFFFF].
+     */
+    private static int add(int x, int y) {
+        return (x + y) & 0xFFFF;
+    }
+
+    /**
+     * Additive inverse in the additive group (mod 2^16).
+     * Range [0, 0xFFFF].
+     */
+    private static int addInv(int x) {
+        return (0x10000 - x) & 0xFFFF;
+    }
+
+    /**
+     * Multiplication in the multiplicative group (mod 2^16+1 = mod 0x10001).
+     * Range [0, 0xFFFF].
+     */
+    private static int mul(int x, int y) {
+        long m = x * y;
+        if (m != 0) {
+            return (int) (m % 0x10001) & 0xFFFF;
+        } else {
+            return (1 - x - y) & 0xFFFF;
+        }
+    }
+
+    /**
+     * Multiplicative inverse in the multiplicative group (mod 2^16+1 = mod 0x10001).
+     * It uses Extended Euclidean algorithm to compute the inverse.
+     * Range [0, 0xFFFF].
+     */
+    private static int mulInv(int x) {
+        if (x <= 1) {
+            // 0 and 1 are their own inverses
+            return x;
+        }
+        int y = 0x10001;
+        int t0 = 1;
+        int t1 = 0;
+        while (true) {
+            t1 += y / x * t0;
+            y %= x;
+            if (y == 1) {
+                return 0x10001 - t1;
+            }
+            t0 += x / y * t1;
+            x %= y;
+            if (x == 1) {
+                return t0;
+            }
+        }
     }
 }
